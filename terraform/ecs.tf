1c98d6be14a3c7c2cd2673efc645ca6d76026678
ccruzado <ccruzado@outlook.com>
2023-05-08 10:20:10 -0500

    upcate module ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,9 +1,18 @@
-module "ecs" {
+module "ecs_cluster" {
   source = "terraform-aws-modules/ecs/aws"
-  name = "ecs-fargate-dvwa"
-  container_insights = true
-  capacity_providers = ["FARGATE"]
-  tags = {
-    Environment = "Development"
+  cluster_name = "ecs-fargate-dvwa"
+
+  # Capacity provider
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
   }
 }
